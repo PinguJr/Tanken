a764796dcf714d1ed8a5c7c51650be4ac2d4ffb4
Lextum <brian.boss@windowslive.com>
2013-11-25 22:23:10 +0100

    last fuckin update today

--- src/Kasse.java
+++ src/Kasse.java
@@ -4,6 +4,7 @@ public class Kasse {
 	
 	List<Tanksaeule> Tanksaeulen = new ArrayList<Tanksaeule>();
 	List<Treibstoff> Treibstoffe = new ArrayList<Treibstoff>();
+	
 	public void Zahlen(int pZapfsaeulenNr){
 		for (Tanksaeule eineTanksaeule : Tanksaeulen) {
 			eineTanksaeule.getmTanksaeulenNr();
