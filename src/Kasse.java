b1e1ac44c770db78208f55ab065c1aeaa18cbc94
Lextum <brian.boss@windowslive.com>
2013-11-25 21:19:57 +0100

    umstrasdasd asd asdasd a

--- src/Kasse.java
+++ src/Kasse.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class Kasse {
 	
 	List<Tanksaeule> Tanksaeulen = new ArrayList<Tanksaeule>();
-	
+	List<Treibstoffpreis> Treibstoffe = new ArrayList<Treibstoffpreis>();
+
 	public void Zahlen(int pZapfsaeulenNr){
 		for (Tanksaeule eineTanksaeule : Tanksaeulen) {
 			eineTanksaeule.getmTanksaeulenNr();
--- src/Treibstoff.java
+++ src/Treibstoff.java
@@ -1,11 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class Treibstoff {
 
 	private String mTreibstoffname;
-	List<Treibstoffpreis> mExPreis = new ArrayList<Treibstoffpreis>();
+	private float mPreis;
+	private Date mDate;
 	
 	public void setmTreibstoffname(String mTreibstoffname) {
 		this.mTreibstoffname = mTreibstoffname;
@@ -14,5 +14,15 @@ public class Treibstoff {
 	public String getmTreibstoffname() {
 		return mTreibstoffname;
 	}
+	
+    public void Treibstoffpreis(float pPreis) {
+    	mDate = new Date();
+    	setmPreis(pPreis);
+    }
+
+	public void setmPreis(float mPreis) {
+		this.mPreis = mPreis;
+	}
+    
 	
 }
